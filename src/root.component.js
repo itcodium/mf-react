bac4f55a6c0c81ea554984a3a20140d0605b224f
unknown <you@example.com>
2025-03-07 16:04:20 -0300

    Zurich Design System was added v2

--- src/root.component.js
+++ src/root.component.js
@@ -1,6 +1,12 @@
 import { useEffect, useRef } from "react";
 import "@zurich/web-components/styles.css";
 import "@zurich/web-components/button";
+import "@zurich/web-components/flag";
+import "@zurich/web-components/toast";
+import { ZrButton } from '@zurich/web-components/react/button';
+import { ZrAlert } from '@zurich/web-components/react/alert';
+
+
 
 function Home(props) {
   const buttonRef = useRef(null);
@@ -25,9 +31,30 @@ function Home(props) {
   return (
     <section>
       <h1>{props.name} is mounted!</h1>
+      <ZrButton config="primary">
+        React
+      </ZrButton>
+      <ZrButton icon="close" />
+      <z-toast
+        custom-str="bg: #06e7a3; color: #043929; radius:5px;"
+      >
+        Custom toast
+      </z-toast>
       <z-button ref={buttonRef} config="primary">
         {props.name}
       </z-button>
+      <z-flag
+        country="ES"
+        custom-str="size:70px"
+      >
+      </z-flag>
+      <ZrAlert
+        confirm-text="Okay"
+        icon="align-text-center"
+      >
+        Alert text
+      </ZrAlert>
+
     </section>
   );
 }
@@ -37,7 +64,27 @@ function App(props) {
     document.title = "React Hash Routing";
   }, []);
 
-  return (<Home {...props} />);
+  return <Home {...props} />;
 }
 
 export default App;
+
+
+/*
+
+     <ZrButton config="primary">
+        {props.name}
+      </ZrButton>
+      <ZrButton config="secondary">
+        {props.name}
+      </ZrButton>
+      <ZrButton config="negative">
+        {props.name}
+      </ZrButton>
+      <ZrButton config="positive">
+        {props.name}
+      </ZrButton>
+      <ZrButton config="link">
+        {props.name}
+      </ZrButton>
+*/
